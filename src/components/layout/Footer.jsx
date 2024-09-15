8da7b35fd4b76f85f16de7d56c1cdf2f7fd5341a
Sasan <sasanfarhadi@gmail.com>
2024-09-15 23:24:07 +0330

    complate.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='container'>
             <div className={styles.footer}>
-                <p>Developed by <a href="https://github.com/sasan-farhadi" target='_blank'>Sasan Farhadi</a></p>
+                <p>Developed by <a href="https://github.com/sasan-farhadi" target='_blank'>Sasan Farhadi.</a></p>
             </div>
         </div>
     )
